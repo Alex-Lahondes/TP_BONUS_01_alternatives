cdd7e41eb91dc0155015131064a6593f5f2acbdd
Alex-Lahondes <alex.lahondes@e.rascol.net>
2023-09-20 17:41:46 +0200

    exo3

--- exo3.cpp
+++ exo3.cpp
@@ -2,5 +2,9 @@
 using namespace std;
 
 int main() {
+    char caractere;
+    cout << "Tapez un caractere : ";
+    cin >> caractere;
+    if (caractere == 'a' or 'e' or 'i' or 'o' or 'u' or 'y');
 
 }
