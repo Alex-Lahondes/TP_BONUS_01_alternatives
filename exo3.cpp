dce967477d99a7ada3594ca76f3c0f5277adc1d3
Alex-Lahondes <alex.lahondes@e.rascol.net>
2023-09-29 11:58:28 +0200

    exo3

--- exo3.cpp
+++ exo3.cpp
@@ -3,8 +3,14 @@ using namespace std;
 
 int main() {
     char caractere;
+
     cout << "Tapez un caractere : ";
     cin >> caractere;
-    if (caractere == 'a' or 'e' or 'i' or 'o' or 'u' or 'y');
-
+    if (caractere == 'a' or 'e' or 'i' or 'o' or 'u' or 'y' or 'A' or 'E' or 'I' or 'O' or 'U' or 'Y') {
+        cout << caractere << " est une voyelle";
+        return 0;}
+    else {
+        cout << caractere << " n'est pas une voyelle";
+        return 1;
+    }
 }
