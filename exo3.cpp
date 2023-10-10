890a288ac41e65b3c13b51b0399b12c3f5a07fdc
Alex-Lahondes <alex.lahondes@e.rascol.net>
2023-10-10 22:03:00 +0200

    Update exo3.cpp

--- exo3.cpp
+++ exo3.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
     char caractere;
+    vector <char> liste{'a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y'};
 
     cout << "Tapez un caractere : ";
     cin >> caractere;
-    if (caractere == 'a' or 'e' or 'i' or 'o' or 'u' or 'y' or 'A' or 'E' or 'I' or 'O' or 'U' or 'Y') {
-        cout << caractere << " est une voyelle";
-        return 0;}
-    else {
-        cout << caractere << " n'est pas une voyelle";
-        return 1;
+
+    int nb = 0;
+
+    while (nb < liste.size()) {
+        if (caractere == liste[nb]) {
+            cout << caractere << " est une voyelle";
+            return 0;}
+        nb = nb+1;
     }
-}
+    cout << caractere << " n'est pas une voyelle";
+    return 1;
+    }
